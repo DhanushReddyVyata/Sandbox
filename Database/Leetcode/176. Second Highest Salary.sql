6f756db0a0baf33f9404de39330ab334c350b045
Dhanush <7468958+DhanushReddyVyata@users.noreply.github.com>
2020-03-11 17:02:31 -0400

    Update 176. Second Highest Salary.sql

--- Database/Leetcode/176. Second Highest Salary.sql
+++ Database/Leetcode/176. Second Highest Salary.sql
@@ -1,8 +1,21 @@
 /* Write your T-SQL query statement below */
-select top 1 Sal.Salary as SecondHighestSalary
-from (
-Select top 2 Salary 
+
+select 
+case 
+when 
+(select count(1) from (
+Select top 2 Salary
 from Employee
+group by Salary
 order by Salary desc
-) as Sal
-order by Sal.Salary
+) as top2Sal) =2 
+Then 
+(select top 1 top2Sal2.Salary from
+                      (select top 2 salary 
+                      from Employee
+                      order by Salary desc) as top2Sal2
+                       order by top2Sal2.Salary asc
+                      )
+
+else null
+end as SecondHighestSalary
