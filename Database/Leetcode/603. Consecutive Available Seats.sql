8328676dcd094c59c467e65246babe6118083ba3
Dhanush <7468958+DhanushReddyVyata@users.noreply.github.com>
2020-04-23 16:35:43 -0400

    4/23 -2

---
+++ Database/Leetcode/603. Consecutive Available Seats.sql
@@ -0,0 +1,35 @@
+/*
+Several friends at a cinema ticket office would like to reserve consecutive available seats.
+Can you help to query all the consecutive available seats order by the seat_id using the following cinema table?
+| seat_id | free |
+|---------|------|
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |
+ 
+
+Your query should return the following result for the sample case above.
+ 
+
+| seat_id |
+|---------|
+| 3       |
+| 4       |
+| 5       |
+Note:
+The seat_id is an auto increment int, and free is bool ('1' means free, and '0' means occupied.).
+Consecutive available seats are more than 2(inclusive) seats consecutively available.
+*/
+
+-- Solution 1 using Window function
+select seat_id 
+from (
+select seat_id, free
+,lag(free, 1) over(order by seat_id) as seat_before
+,lead(free, 1) over(order by seat_id) as seat_after
+from cinema
+) s
+where (free = 1 and seat_before = 1) 
+or (free = 1 and seat_after = 1)
